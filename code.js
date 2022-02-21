144d39c5bafe028a7cd570c16ca5d3266c925a89
Clayton <claytongorham@gmail.com>
2022-02-20 19:08:01 -0800

    include data-index, add more javascript functionality

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 
-//declare our "cells" variable as a query selector for each cell on the board
+//declare our "cells". document.queryselector for ALL cells on the board
 const cells = document.querySelectorAll(".cell");
 
 
@@ -7,13 +7,36 @@ const cells = document.querySelectorAll(".cell");
 const player_x = "x";
 const player_o = "o";
 
-// "turn" defined as player x
+// track turns as let, not as a const, because it will change. starting as player x.
 let turn = player_x;
 
-// define gameboard as our array with cells.length
+// define gameboard as our array as cells.length
 const gameboard = Array(cells.length);
-gameboard.fill(null);
+gameboard.fill(null);  // fill in the gameboard array with values of null
+//console.log(gameboard);
+
+
+//HTML elements
+const turnMessage = document.getElementsByClassName("turn-message");
+const resetButton = document.getElementById("reset");
 
 
 
+//define variables for audio
+const shortBeep = new Audio("audio/beep-short.mp3");
+const longBeep = new Audio("audio/clockbeep.mp3");
+const winSound = new Audio("audio/tada.mp3");
+const tieSound = new Audio("audio/tie.wav");
 
+
+// add event listeners for each cell on the board
+cells.forEach((cell) => cell.addEventListener('click', cellClick))
+
+//define cellClick function. event gives us which box was clicked.
+function cellClick(event){
+    if(gameOverArea.classList.contains('visible'))  {       //if game over area class list contains visible (x or o), then return to stop function.
+        return; 
+    }
+
+    const cell = event.target;
+    const cellNumber = cell.
