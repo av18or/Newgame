cc4df60274f42c5983dde1126079be0b5f6e91dc
Clayton <claytongorham@gmail.com>
2022-02-20 18:14:19 -0800

    begin javascript, style updates

--- code.js
+++ code.js
@@ -1,3 +1,19 @@
 
-let cell = document.getElementById();
+//declare our "cells" variable as a query selector for each cell on the board
+const cells = document.querySelectorAll(".cell");
+
+
+// define player X and player O
+const player_x = "x";
+const player_o = "o";
+
+// "turn" defined as player x
+let turn = player_x;
+
+// define gameboard as our array with cells.length
+const gameboard = Array(cells.length);
+gameboard.fill(null);
+
+
+
 
